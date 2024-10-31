a12350b22c1926994c2d49f7d1822057c1613f1f
ThuTrang21 <thut15794@gmail.com>
2024-11-01 00:16:50 +0700

    update

--- src/component/Home/CarouselItem.jsx
+++ src/component/Home/CarouselItem.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+  import React from 'react'
 
-export default function CarouselItem({image,title}) {
-  return (
-    <div className='flex flex-col justify-center items-center py-7 px-7'>
-       <img className='w-[12rem] h-[12rem] lg:w-[12rem] rounded-full object-cover object-center' src={image} alt=''></img>
-    <span className='py-5 font-semibold text-xl text-gray-400'>{title}</span>
+  export default function CarouselItem({image,title}) {
+    return (
+      <div className='flex flex-col justify-center items-center py-7 px-7'>
+      <div className='w-[4rem] h-[4rem] md:w-[12rem] md:h-[12rem] sm:w-[6rem] sm:h-[6rem] rounded-full overflow-hidden'>
+        <img 
+          className='w-full h-full object-cover object-center' 
+          src={image} 
+          alt='' 
+        />
+      </div>
+      <span className='py-5 font-semibold text-xl text-gray-400'>{title}</span>
     </div>
-  )
-}
+    )
+  }
